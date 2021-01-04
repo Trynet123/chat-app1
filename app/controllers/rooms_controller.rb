9c18161667732d6708e9ad37a64409726d15ede6
Trynet123 <su508102@yahoo.co.jp>
2021-01-05 07:01:41 +0900

    Active Storageの導入

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -18,11 +18,8 @@ class RoomsController < ApplicationController
 
   def destroy
     room = Room.find(params[:id])
-    # Room.find(params[:id])を使用して、削除したいチャットルームの情報を取得
-    # Roomのどの部屋ですか？と聞いて、答えをroomに入れている。
     room.destroy
     redirect_to root_path
-    # root（roomsのindex）にリダイレクト
 
     # @room.destroy
     # 削除するだけなのでビューの表示は必要はありません。
