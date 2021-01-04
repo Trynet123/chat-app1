c30ee878c4afcb4dd0b7a86502024156c35e50db
Trynet123 <su508102@yahoo.co.jp>
2021-01-04 14:20:03 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   # ビューを実装するために仮のルーティング
-  root to: "messages#index"
+  root to: "rooms#index"
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
 end
